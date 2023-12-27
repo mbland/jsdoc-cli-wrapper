ed8920fc224b43d1f10d2effe872ad350a7ab5b4
Mike Bland <mbland@acm.org>
2023-12-26 20:29:27 -0500

    Add getPath test, lint scripts
    Introduces a test/fixtures/index.js convention that should prove handy
    for the remaining tests.
    
    I thought about using memfs, but I'm going to see how far I can get with
    this method first.

---
+++ test/fixtures/index.js
@@ -0,0 +1,39 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import path from 'node:path'
+
+/**
+ * Returns the absolute path to a test fixture directory.
+ * @param {string} fixtureName - test fixture name
+ * @returns {string} - the absolute path to the test fixture directory
+ */
+export function fixturePath(fixtureName) {
+  return new URL(fixtureName, import.meta.url).pathname
+}
+
+/**
+ * A fake environment variable object
+ * @typedef {object} FakeEnv
+ * @property {string} PATH - platform-correct PATH variable
+ */
+
+/**
+ * Returns an environment object containing PATH set for the test fixture.
+ *
+ * The PATH will always contain ['usr/local/bin', 'usr/bin', 'bin'] relative to
+ * the fixture root. It will be constructed using the Node.js path module to
+ * ensure it's native to the host platform.
+ * @param {string} fixtureName - test fixture name
+ * @returns {FakeEnv} - a fake environment for the specified test fixture
+ */
+export function fixtureEnv(fixtureName) {
+  const root = fixturePath(fixtureName)
+  const paths = [
+    path.join('usr', 'local', 'bin'), path.join('usr', 'bin'), 'bin'
+  ]
+  return { PATH: paths.map(p => path.join(root, p)).join(path.delimiter) }
+}
---
+++ test/getPath.test.js
@@ -0,0 +1,31 @@
+/* eslint-env vitest */
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+
+import { getPath } from '../lib/index.js'
+import { fixturePath, fixtureEnv } from './fixtures/index.js'
+import { describe, expect, test } from 'vitest'
+import path from 'node:path'
+
+describe('getPath', () => {
+  const root = fixturePath('getPath')
+  const env = fixtureEnv('getPath')
+
+  test('finds command on POSIX system', async() => {
+    await expect(getPath('testcmd', env, 'linux')).resolves
+      .toBe(path.join(root, 'bin', 'testcmd'))
+  })
+
+  test('finds command on Windows system', async() => {
+    await expect(getPath('testcmd', env, 'win32')).resolves
+      .toBe(path.join(root, 'usr', 'bin', 'testcmd.CMD'))
+  })
+
+  test('rejects when command isn\'t found', async () => {
+    await expect(getPath('nonexistent', env, process.platform)).rejects
+      .toBe('nonexistent not found in PATH')
+  })
+})
